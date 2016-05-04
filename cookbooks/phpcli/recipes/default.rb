a8830ffe204519c481837f98cfc321155df97bec
Javier C <aye.javier@gmail.com>
2016-05-04 01:31:37 -0300

    REFACTOR: agregado clone git

--- cookbooks/phpcli/recipes/default.rb
+++ cookbooks/phpcli/recipes/default.rb
@@ -15,3 +15,25 @@ end
 package ["git"]  do
   action :install
 end
+git "/vagrant" do
+#  repository: "https://github.com/AyeJavier/coding_dojo_phpunit.git"
+#  action :checkout
+
+      #retries 0
+      #retry_delay 2
+      #default_guard_interpreter :default
+      #destination "/vagrant"
+      #enable_checkout true
+      #revision "HEAD"
+      #remote "origin"
+      #checkout_branch "deploy"
+      #declared_type :git
+      #cookbook_name :phpcli
+      #recipe_name "default"
+      #repository "git@github.com:AyeJavier/coding_dojo_phpunit.git"
+      repository 'git://github.com/sstephenson/ruby-build.git'
+      #enable_checkout false
+      revision 'master'
+      action :sync
+
+end
